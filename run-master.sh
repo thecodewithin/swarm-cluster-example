c97580841f9ec0a844f32ab09faadad3e4489f7d
thecodewithin <thecodewithin@protonmail.com>
2020-10-23 13:52:12 +0000

    Initial commit

--- run-master.sh
+++ run-master.sh
@@ -6,21 +6,21 @@ mkdir -p /srv/swarm
 
 # Configraciói NFS
 
-#apt install -y nfs-kernel-server
+apt install -y nfs-kernel-server
 
-echo '/sev/nfs 10.132.0.0/24(rw,no_root_swash,no_subtree_check)' >> /etc/exports
+echo '/srv/nfs 10.132.0.0/24(rw,no_root_swash,no_subtree_check)' >> /etc/exports
 
 echo 'master01:/srv/nfs /srv/swarm nfs defaults,nfsver=3 0 0' >> /etc/fstab
 
-apt install -y nfs-kernel-server
-#systemctl restart nfs-kenel-server
+systemctl enable nfs-kenel-server
+systemctl start nfs-kenel-server
 mount  -a
 
 # configuració del Swarm
 
 docker swarm init
 docker swarm join-token manager | grep join > /srv/swarm/join.sh
-chmod +x $!
+chmod +x /srv/swarm/join.sh
 
 cd /srv/swarm
 git clone https://github.com/thecodewithin/swarm-cluster-example
