79b41aeeedcc9aa47e8beff027083096597e55d9
root <root@master01.europe-west1-b.c.trusty-splice-293316.internal>
2020-10-28 10:08:21 +0000

    Initial commit

--- run-deploy.sh
+++ run-deploy.sh
@@ -4,9 +4,9 @@
 #IP2=$(getent hosts master02 | awk '{print $1}')
 #IP3=$(getent hosts master03 | awk '{print $1}')
 
-IP1=34.77.5.19
-IP2=34.77.123.11
-IP3=35.195.212.147
+IP1=35.195.175.232
+IP2=34.76.252.233
+IP3=35.187.160.144
 
 export IP1 
 export IP2 
@@ -17,3 +17,4 @@ cd /srv/swarm/swarm-cluster-example
 docker stack deploy -c traefik/stack.yaml traefik
 docker stack deploy -c portainer/stack.yaml portainer
 docker stack deploy -c my-wordpress/stack.yml my-wordpress
+docker stack deploy -c logspout-elk/stack.yml logspout-elk
